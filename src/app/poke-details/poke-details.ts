c7331c2b3e81ccf815414e5e25160e46b7ac4a5c
Islomov-Doniyorbek <islomovdoniyorbey@gmail.com>
2026-06-04 14:03:27 +0500

    make:layout and structure

---
+++ src/app/poke-details/poke-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-details',
+  imports: [],
+  templateUrl: './poke-details.html',
+  styleUrl: './poke-details.css',
+})
+export class PokeDetails {}
---
+++ src/app/pokelist/pokelist/pokelist.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-pokelist',
+  imports: [RouterLink],
+  templateUrl: './pokelist.html',
+  styleUrl: './pokelist.css',
+})
+export class Pokelist {}
