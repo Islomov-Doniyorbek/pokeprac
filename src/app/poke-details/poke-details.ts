3aa9c5020bcb32e311f87f47c87b789382996f73
Islomov-Doniyorbek <islomovdoniyorbey@gmail.com>
2026-06-05 10:34:16 +0500

    poke-details

--- src/app/poke-details/poke-details.ts
+++ src/app/poke-details/poke-details.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PokeService } from '../services/poke.service';
+import { PokeAbilities } from '../models/pokemon';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-poke-details',
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './poke-details.html',
   styleUrl: './poke-details.css',
 })
-export class PokeDetails {}
+export class PokeDetails implements OnInit {
+  activatedRoute = inject(ActivatedRoute)
+  pokkeService = inject(PokeService)
+  router = inject(Router)
+  cdr = inject(ChangeDetectorRef)
+
+
+  pokeDetails:PokeAbilities | null = null
+
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id')
+    if(!id) return;
+    this.pokkeService.getPokeById(+id).subscribe({
+      next: (data)=>{
+        console.log(data);
+        this.pokeDetails = {
+          id: data.id,
+          order: data.order,
+          base_experience: data.base_experience,
+          title: data.species.name,
+          height: data.height,
+          weight: data.weight,
+          hp: data.stats[0].base_stat,
+          attacks: data.stats[1].base_stat,
+          defence: data.stats[2].base_stat,
+          special_attacks: data.stats[3].base_stat,
+          special_defence: data.stats[4].base_stat,
+          speed: data.stats[5].base_stat,
+        }
+
+        this.cdr.detectChanges();
+      }
+    })
+  }
+
+}
